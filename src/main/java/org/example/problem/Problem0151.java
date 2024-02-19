b21873f76cb785497c7839a7ff7dd94774cedd32
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-02-19 13:57:00 -0600

    Problem 151 - Append space between words

--- src/main/java/org/example/problem/Problem0151.java
+++ src/main/java/org/example/problem/Problem0151.java
@@ -12,6 +12,8 @@ public class Problem0151 {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = words.length - 1; i >= 0; i--) {
             stringBuilder.append(words[i]);
+            if(i > 0)
+                stringBuilder.append(" ");
         }
         return stringBuilder.toString();
     }

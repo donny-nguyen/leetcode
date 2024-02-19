392a7fce969b54073b4b2f6a0fab0cf71da3ade1
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-02-19 13:51:09 -0600

    Problem 151 - Add test cases

--- src/main/java/org/example/problem/Problem0151.java
+++ src/main/java/org/example/problem/Problem0151.java
@@ -2,6 +2,12 @@ package org.example.problem;
 
 public class Problem0151 {
     public Problem0151() {
-        System.out.println("Problem0151");
+        System.out.println(reverseWords("the sky is blue").equals("blue is sky the"));
+        System.out.println(reverseWords("  hello world  ").equals("world hello"));
+        System.out.println(reverseWords("a good   example").equals("example good a"));
+    }
+
+    public String reverseWords(String s) {
+        return "";
     }
 }

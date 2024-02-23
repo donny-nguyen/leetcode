32f4515ddb4f8a7c15967b389497a1c2b8c9edfd
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-02-23 15:32:39 -0600

    Problem 338 - Add test cases

--- src/main/java/org/example/problem/Problem0338.java
+++ src/main/java/org/example/problem/Problem0338.java
@@ -2,6 +2,17 @@ package org.example.problem;
 
 public class Problem0338 {
     public Problem0338() {
-        System.out.println("Problem0338");
+        int[] ans = CountBits(2);
+        for (int i : ans)
+            System.out.print(i + " ");
+        System.out.println();
+
+        ans = CountBits(5);
+        for (int i : ans)
+            System.out.print(i + " ");
+    }
+
+    public int[] CountBits(int n) {
+        return new int[0];
     }
 }

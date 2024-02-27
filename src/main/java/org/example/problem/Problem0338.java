d86a83a2fa8cddfd5a0abe0c30614c289abfc4f2
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-02-27 14:27:47 -0600

    Problem 338 - Optimize the solution

--- src/main/java/org/example/problem/Problem0338.java
+++ src/main/java/org/example/problem/Problem0338.java
@@ -18,59 +18,18 @@ public class Problem0338 {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
         for (int i = 0; i < ans.length; i++) {
-            List<Integer> bin = binary(i);
-            ans[i] = countOne(bin);
+            ans[i] = countOne(i);
         }
         return ans;
     }
 
-    private List<Integer> binary(int i) {
-        List<Integer> bin = new ArrayList<>();
-        while(i > 0){
-            int mod = i % 2;
-            bin.add(mod);
-            i /= 2;
-        }
-        return bin;
-    }
-
-    private int countOne(List<Integer> bin) {
+    private int countOne(int num) {
         int countOne = 0;
-        for(int e : bin) {
-            if(e == 1)
+        while (num > 0) {
+            if ((num & 1) == 1)
                 countOne++;
+            num = num >> 1;
         }
         return countOne;
     }
 }
-
-// n = 2
-// ans = {_, _, _}
-// i = 0
-//      b = binary(i) = 0
-//      countOne(b) = 0
-//      ans = [0, _, _}
-// i = 1
-//      b = binary(i) = 1
-//      countOne(b) = 1
-//      ans = [0, 1, _}
-// i = 2
-//      b = binary(i) = 10
-//      countOne(b) = 1
-//      ans = [0, 1, 1}
-
-// binary(i)
-// i = 5
-// bin = {}
-// r = i / 2 = 2
-//      mod = i - r * 2 = 5 - 2 * 2 = 5 - 4 = 1
-//      bin = {1}
-//      i = r = 2
-// r = i / 2 = 2 / 2 = 1
-//      mod = i - r * 2 = 2 - 1 * 2 = 2 - 2 = 0
-//      bin = {1, 0}
-//      i = r = 1
-// r = i / 2 = 1 / 2 = 0
-//      mod = i - r * 2 = 1 - 0 * 2 = 1 - 0 = 1
-//      bin = {1, 0, 1}
-//      i = r = 0

d55f12b5f36ed6a377b25238a0aaf8c7e39658f7
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-02-28 17:16:15 -0600

    Problem 375 - Add test cases

--- src/main/java/org/example/problem/Problem0375.java
+++ src/main/java/org/example/problem/Problem0375.java
@@ -2,6 +2,11 @@ package org.example.problem;
 
 public class Problem0375 {
     public Problem0375() {
-        System.out.println("Problem0375");
+        System.out.println(getMoneyAmount(10) == 16);
+        System.out.println(getMoneyAmount(2) == 1);
+    }
+
+    public int getMoneyAmount(int n) {
+        return 0;
     }
 }

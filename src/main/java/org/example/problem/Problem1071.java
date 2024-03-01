c1c43b7652a0444a099caa66e3f7b006dd1584f6
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-01 16:19:24 -0600

    Problem 1071 - Add test cases

--- src/main/java/org/example/problem/Problem1071.java
+++ src/main/java/org/example/problem/Problem1071.java
@@ -2,6 +2,12 @@ package org.example.problem;
 
 public class Problem1071 {
     public Problem1071() {
-        System.out.println("Problem1071");
+        System.out.println(gcdOfStrings("ABCABC", "ABC").equals("ABC"));
+        System.out.println(gcdOfStrings("ABABAB", "ABAB").equals("AB"));
+        System.out.println(gcdOfStrings("LEET", "CODE").equals(""));
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+        return "";
     }
 }

5f8d1dc1beba0ad7af78f4a524d579b4acb48935
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-01 16:40:18 -0600

    Problem 1071 - Try to resolve

--- src/main/java/org/example/problem/Problem1071.java
+++ src/main/java/org/example/problem/Problem1071.java
@@ -8,6 +8,42 @@ public class Problem1071 {
     }
 
     public String gcdOfStrings(String str1, String str2) {
+        String x = str1.length() < str2.length() ? str1 : str2;
+
+        while (!x.isEmpty()) {
+            if (canDevide(str1, x) && canDevide(str2, x))
+                return x;
+            if (x.length() % 2 == 0)
+                x = x.substring(0, x.length() / 2);
+            else
+                return "";
+        }
+
         return "";
     }
+
+    private boolean canDevide(String str, String devider) {
+        if (str.length() % devider.length() != 0)
+            return false;
+
+        for (int i = 0; i < str.length(); i += devider.length()) {
+            if (str.substring(i, devider.length()).equals(devider) == false)
+                return false;
+        }
+
+        return true;
+    }
 }
+
+// str1 = "ABCABC", str2 = "ABC"
+// x = str2 = "ABC"
+//      str1.length() % x.length() == 0
+//      x.equals(str1.substring(i,x.length())
+// x = ""
+
+// str1 = "ABABAB", str2 = "ABAB"
+// x = str2 = "ABAB"
+//      str1.length() % x.length() = 6 % 4 = 2 != 0
+// x = x / 2 = "AB"
+//      str1.length() % x.length() = 6 % 2 = 0 == 0
+//      x.equals(str1.substring(i,x.length())

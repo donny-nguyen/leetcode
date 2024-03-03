aa597a2e856d6792a86681c4f7843ae00c5cca79
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-02 22:49:13 -0600

    Problem 1071 - Optimize the solutions

--- src/main/java/org/example/problem/Problem1071.java
+++ src/main/java/org/example/problem/Problem1071.java
@@ -9,42 +9,14 @@ public class Problem1071 {
     }
 
     public String gcdOfStrings(String str1, String str2) {
-        String shorterStr = str1.length() < str2.length() ? str1 : str2;
-        String x = shorterStr;
+        if (str1.isEmpty()) return str2;
+        if (str2.isEmpty()) return str1;
 
-        while (!x.isEmpty()) {
-            if (canDevide(str1, x) && canDevide(str2, x))
-                return x;
-            do {
-                x = x.substring(0, x.length() - 1);
-            } while (!x.isEmpty() && shorterStr.length() % x.length() != 0);
-        }
-        return x;
-    }
+        if (str1.startsWith(str2))
+            return gcdOfStrings(str1.substring(str2.length()), str2);
+        if (str2.startsWith(str1))
+            return gcdOfStrings(str2.substring(str1.length()), str1);
 
-    private boolean canDevide(String str, String devider) {
-        if (str.length() % devider.length() != 0)
-            return false;
-
-        for (int i = 0; i < str.length(); i += devider.length()) {
-            String subStringOfStr = str.substring(i, i + devider.length());
-            if (subStringOfStr.equals(devider) == false)
-                return false;
-        }
-
-        return true;
+        return "";
     }
 }
-
-// str1 = "ABCABC", str2 = "ABC"
-// x = str2 = "ABC"
-//      str1.length() % x.length() == 0
-//      x.equals(str1.substring(i,x.length())
-// x = ""
-
-// str1 = "ABABAB", str2 = "ABAB"
-// x = str2 = "ABAB"
-//      str1.length() % x.length() = 6 % 4 = 2 != 0
-// x = x / 2 = "AB"
-//      str1.length() % x.length() = 6 % 2 = 0 == 0
-//      x.equals(str1.substring(i,x.length())

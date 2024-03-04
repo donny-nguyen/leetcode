b1c29b8756f9edc06d34f8f18aa9ee53563414df
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-04 16:33:45 -0600

    Problem 326 - Fix more test cases

--- src/main/java/org/example/problem/Problem0326.java
+++ src/main/java/org/example/problem/Problem0326.java
@@ -5,14 +5,15 @@ public class Problem0326 {
         System.out.println(isPowerOfThree(27) == true);
         System.out.println(isPowerOfThree(0) == false);
         System.out.println(isPowerOfThree(-1) == false);
+        System.out.println(isPowerOfThree(1) == true);
     }
 
     public boolean isPowerOfThree(int n) {
         if (n == 0) return false;
+        if (n == 1) return true;
+        if (n == -1) return false;
 
         n = Math.abs(n);
-        if (n == 1) return false;
-
         while (n != 1 && n % 3 == 0) {
             n /= 3;
         }

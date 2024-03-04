0278fb0b00b54922bc4214efea93edfb9312f9b8
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-04 16:15:05 -0600

    Problem 326 - Add test cases

--- src/main/java/org/example/problem/Problem0326.java
+++ src/main/java/org/example/problem/Problem0326.java
@@ -1,7 +1,13 @@
 package org.example.problem;
 
 public class Problem0326 {
-    public Problem0326(){
-        System.out.println("Problem0326");
+    public Problem0326() {
+        System.out.println(isPowerOfThree(27) == true);
+        System.out.println(isPowerOfThree(0) == false);
+        System.out.println(isPowerOfThree(-1) == false);
+    }
+
+    public boolean isPowerOfThree(int n) {
+        return false;
     }
 }

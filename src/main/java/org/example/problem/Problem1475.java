ebc83f5b5b98792afb5f558c96b4a3585266bee5
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-06 11:04:39 -0600

    Problem 1475 - Optimize memory

--- src/main/java/org/example/problem/Problem1475.java
+++ src/main/java/org/example/problem/Problem1475.java
@@ -23,18 +23,14 @@ public class Problem1475 {
     }
 
     public int[] finalPrices(int[] prices) {
-        int[] answer = new int[prices.length];
-
         for (int i = 0; i < prices.length; i++) {
-            answer[i] = prices[i];
             for (int j = i + 1; j < prices.length; j++) {
                 if (prices[j] <= prices[i]) {
-                    answer[i] -= prices[j];
+                    prices[i] -= prices[j];
                     break;
                 }
             }
         }
-
-        return answer;
+        return prices;
     }
 }

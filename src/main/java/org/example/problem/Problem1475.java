3b66eb8f0e3031490bd17edcec2f0e9e1de80fe6
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-06 10:49:04 -0600

    Problem 1475 - Add test cases

--- src/main/java/org/example/problem/Problem1475.java
+++ src/main/java/org/example/problem/Problem1475.java
@@ -2,6 +2,27 @@ package org.example.problem;
 
 public class Problem1475 {
     public Problem1475() {
-        System.out.println("Problem1475");
+        int[] answer = finalPrices(new int[]{8, 4, 6, 2, 3});
+        // Expected: 4,2,4,2,3
+        for (int price : answer)
+            System.out.print(price + " ");
+
+        System.out.println();
+
+        answer = finalPrices(new int[]{1, 2, 3, 4, 5});
+        // Expected: 1,2,3,4,5
+        for (int price : answer)
+            System.out.print(price + " ");
+
+        System.out.println();
+
+        answer = finalPrices(new int[]{10, 1, 1, 6});
+        // Expected: 9,0,1,6
+        for (int price : answer)
+            System.out.print(price + " ");
+    }
+
+    public int[] finalPrices(int[] prices) {
+        return new int[]{0};
     }
 }

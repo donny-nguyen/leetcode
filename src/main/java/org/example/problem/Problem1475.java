377c1aeef57ea38be3832088ef324cfb0702f158
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-06 11:01:40 -0600

    Problem 1475 - Resolve

--- src/main/java/org/example/problem/Problem1475.java
+++ src/main/java/org/example/problem/Problem1475.java
@@ -23,6 +23,18 @@ public class Problem1475 {
     }
 
     public int[] finalPrices(int[] prices) {
-        return new int[]{0};
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            answer[i] = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    answer[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+
+        return answer;
     }
 }

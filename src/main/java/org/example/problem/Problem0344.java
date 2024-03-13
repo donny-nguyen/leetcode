c464615775814c9406092430d7d3477ee192f851
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-13 14:54:05 -0500

    Problem 344 - Add the test case

--- src/main/java/org/example/problem/Problem0344.java
+++ src/main/java/org/example/problem/Problem0344.java
@@ -2,6 +2,17 @@ package org.example.problem;
 
 public class Problem0344 {
     public Problem0344() {
-        System.out.println("Problem0344");
+        char[] s = new char[]{'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        for (char c : s)
+            System.out.print(c);
+        System.out.println();
+        s = new char[]{'H', 'a', 'n', 'n', 'a', 'h'};
+        for (char c : s)
+            System.out.print(c);
+    }
+
+    public void reverseString(char[] s) {
+
     }
 }

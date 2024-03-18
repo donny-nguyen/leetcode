415abb7def96239eb39e93651f0a295f1b2b8819
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-18 14:09:04 -0500

    Problem 118 - Add test cases

--- src/main/java/org/example/problem/Problem0118.java
+++ src/main/java/org/example/problem/Problem0118.java
@@ -1,7 +1,30 @@
 package org.example.problem;
 
+import java.util.List;
+
 public class Problem0118 {
     public Problem0118() {
-        System.out.println("Problem0118");
+        System.out.println(generate(5));
+        System.out.println(generate(1));
+    }
+
+    private String resultToString(List<List<Integer>> result) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < result.size(); i++) {
+            stringBuilder.append("[");
+            for (int j = 0; j < result.get(i).size(); j++) {
+                stringBuilder.append(result.get(i).get(j));
+                if (i < result.get(i).size() - 1)
+                    stringBuilder.append(",");
+            }
+            stringBuilder.append("]");
+            if (i < result.size() - 1)
+                stringBuilder.append(",");
+        }
+        return stringBuilder.toString();
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        return null;
     }
 }

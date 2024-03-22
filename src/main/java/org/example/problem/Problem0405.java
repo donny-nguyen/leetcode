3448fea6b966d2a974701685faeee2e49e18526a
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-22 15:10:38 -0500

    Problem 405 - Convert positive number

--- src/main/java/org/example/problem/Problem0405.java
+++ src/main/java/org/example/problem/Problem0405.java
@@ -7,6 +7,27 @@ public class Problem0405 {
     }
 
     public String toHex(int num) {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        while (num > 0) {
+            int digit = num & 0xf;
+            char hexChar = digitToHexChar(digit);
+            stringBuilder.append(hexChar);
+            num >>= 4;
+        }
+        return stringBuilder.reverse().toString();
+    }
+
+    private char digitToHexChar(int digit) {
+        if (digit < 10)
+            return (char) ('0' + digit);
+        return (char) ('a' + (digit - 10));
     }
 }
+
+// num          26      0001
+// num & 0xf    1010    0001
+// bin          [0001, 1010]
+
+// digit        10              11              12
+// return       'a'             'b'             'c'
+// result       'a'+(10-10)     'a'+(11-10)     'a'+(12-10)

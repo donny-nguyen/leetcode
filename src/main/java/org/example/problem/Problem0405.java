3789208e2d9f4a45dd55a15fc9e81d217f3624a7
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-22 14:56:13 -0500

    Problem 405 - Add test cases

--- src/main/java/org/example/problem/Problem0405.java
+++ src/main/java/org/example/problem/Problem0405.java
@@ -2,6 +2,11 @@ package org.example.problem;
 
 public class Problem0405 {
     public Problem0405() {
-        System.out.println("Problem0405");
+        System.out.println(toHex(26).equals("1a"));
+        System.out.println(toHex(-1).equals("ffffffff"));
+    }
+
+    public String toHex(int num) {
+        return "";
     }
 }

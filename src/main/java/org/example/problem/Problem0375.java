508c2516750d7271fced43ff37c4f90632a59f63
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-23 12:10:40 -0500

    Problem 375 - Resolve by dynamic programming

--- src/main/java/org/example/problem/Problem0375.java
+++ src/main/java/org/example/problem/Problem0375.java
@@ -7,48 +7,22 @@ public class Problem0375 {
     }
 
     public int getMoneyAmount(int n) {
-        return getMoneyAmount(1, n);
-    }
-
-    private int getMoneyAmount(int min, int max) {
-        int n = max - min + 1;
-
-        int[] numbers = new int[n];
-        for (int i = 0, j = min; i < n; i++, j++) {
-            numbers[i] = j;
-        }
-
-        int[] leftSum = new int[n];
-        for (int i = 1; i < n; i++) {
-            leftSum[i] = leftSum[i - 1] + numbers[i - 1];
-        }
-
-        int[] rightSum = new int[n];
-        for (int i = n - 2; i >= 0; i--) {
-            rightSum[i] = rightSum[i + 1] + numbers[i + 1];
-        }
-
-        int[] differences = new int[n];
-        for (int i = 0; i < n; i++) {
-            differences[i] = Math.abs(leftSum[i] - rightSum[i]);
-        }
-
-        int minIndex = minIndex(differences);
-        int leftMin = minIndex > 0 ? getMoneyAmount(min, numbers[minIndex - 1]) : 0;
-        int rightMin = minIndex < (n - 1) ? getMoneyAmount(numbers[minIndex + 1], max) : 0;
-
-        return leftMin < rightMin ? numbers[minIndex] + rightMin : numbers[minIndex] + leftMin;
-    }
-
-    private int minIndex(int[] array) {
-        int minIndex = 0;
-        int minValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue) {
-                minIndex = i;
-                minValue = array[i];
+        int[][] minMoney = new int[n + 2][n + 1];
+        for (int rangeLength = 2; rangeLength <= n; rangeLength++) {
+            for (int start = 1; start <= (n - rangeLength + 1); start++) {
+                int end = start + rangeLength - 1;
+                int minMoneyAtThisRange = Integer.MAX_VALUE;
+                for (int selectedNumber = start; selectedNumber <= end; selectedNumber++) {
+                    int leftMoney = minMoney[start][selectedNumber - 1];
+                    int rightMoney = minMoney[selectedNumber + 1][end];
+                    int minMoneyWithSelectedNumber = selectedNumber + Math.max(leftMoney, rightMoney);
+                    if (minMoneyWithSelectedNumber < minMoneyAtThisRange) {
+                        minMoneyAtThisRange = minMoneyWithSelectedNumber;
+                    }
+                }
+                minMoney[start][end] = minMoneyAtThisRange;
             }
         }
-        return minIndex;
+        return minMoney[1][n];
     }
 }

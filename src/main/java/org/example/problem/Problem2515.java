78069b34a84a3f321807e3c53a3c8278853a0682
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-26 13:54:56 -0500

    Problem 2515 - Add test cases

--- src/main/java/org/example/problem/Problem2515.java
+++ src/main/java/org/example/problem/Problem2515.java
@@ -2,6 +2,12 @@ package org.example.problem;
 
 public class Problem2515 {
     public Problem2515() {
-        System.out.println("Problem2515");
+        System.out.println(closetTarget(new String[]{"hello", "i", "am", "leetcode", "hello"}, "hello", 1) == 1);
+        System.out.println(closetTarget(new String[]{"a", "b", "leetcode"}, "leetcode", 0) == 1);
+        System.out.println(closetTarget(new String[]{"i", "eat", "leetcode"}, "ate", 0) == -1);
+    }
+
+    public int closetTarget(String[] words, String target, int startIndex) {
+        return -2;
     }
 }

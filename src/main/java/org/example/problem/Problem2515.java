e672394c742824df50f9f14cf6f053f1d7c42f35
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-26 14:13:38 -0500

    Problem 2515 - Resolve

--- src/main/java/org/example/problem/Problem2515.java
+++ src/main/java/org/example/problem/Problem2515.java
@@ -8,6 +8,41 @@ public class Problem2515 {
     }
 
     public int closetTarget(String[] words, String target, int startIndex) {
-        return -2;
+        if (words[startIndex].equals(target))
+            return 0;
+        int shortestDistance = 0;
+        int loopCount = words.length / 2;
+        for (int i = 0, j = (startIndex + 1) % words.length, k = (startIndex - 1 + words.length) % words.length; i < loopCount; i++, j++, k++) {
+            shortestDistance++;
+            if (words[j].equals(target) || words[k].equals(target))
+                return shortestDistance;
+        }
+        return -1;
     }
 }
+
+//                        0        1    2     3           4
+// words                {"hello", "i", "am", "leetcode", "hello"}
+// startIndex                      ^
+// words[startIndex]    "i"
+// i                      ^
+// words[i]             "i"     "hello"
+// count                1
+
+//                        0    1    2
+// words                {"a", "b", "leetcode"}
+// startIndex             ^
+// words[startIndex]    "a"
+// i                                    ^
+// words[i]             "leetcode"
+// count                1
+
+//                        0    1      2                         length      loopCount
+// words                {"i", "eat", "leetcode"}                3           3/2=1
+// startIndex             ^
+// words[startIndex]    "i"
+// i                            ^
+// words[i]             "leetcode"      "eat"
+// j                                    ^
+// words[j]             "eat"           "leetcode"
+// count                1   2

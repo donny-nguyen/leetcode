ca2ebe3c73d939b6b64cd0f8161f032f916141b4
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-03-27 17:03:00 -0500

    Problem 1832 - Add test cases

--- src/main/java/org/example/problem/Problem1832.java
+++ src/main/java/org/example/problem/Problem1832.java
@@ -2,6 +2,11 @@ package org.example.problem;
 
 public class Problem1832 {
     public Problem1832() {
-        System.out.println("Problem1832");
+        System.out.println(checkIfPangram("thequickbrownfoxjumpsoverthelazydog") == true);
+        System.out.println(checkIfPangram("leetcode") == false);
+    }
+
+    public boolean checkIfPangram(String sentence) {
+        return false;
     }
 }
